e9c0935ba5b7fedca01245dc5ebde39d045df89a
Muhammad Fikri Maulana <mfikrimaulana131203@example.com>
2025-02-20 08:59:02 +0800

    Mengedit Kode View dan Web

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/antrian', [AntrianPasienController::class, 'antrianPasien']);
+Route::get('/antrian', [AntrianPasienController::class, 'antrianPasien'])->name('antrian');
 
 // Route::get('/pasien', function () {
 //     return view('dataPasien');
@@ -21,8 +21,7 @@ Route::get('/pasien', [DataPasienController::class, 'tampilkanAntrian'])->name('
 Route::post('/cari-pasien', [DataPasienController::class, 'cariPasien'])->name('cari.pasien');
 
 Route::get('/get-antrian', [DataPasienController::class, 'tampilkanAntrian'])->name('tampilkan.antrian');
-Route::get('/data-antrian', [DataPasienController::class, 'tampilkanAntrian'])->name('data.antrian'); // Route ini redundant, bisa dihapus jika tidak perlu
+Route::get('/data-antrian', [DataPasienController::class, 'tampilkanAntrian'])->name('data.antrian');
 Route::post('/panggil/{nomor_rm}', [DataPasienController::class, 'panggilPasien'])->name('panggil.pasien');
 Route::post('/selesai/{nomor_rm}', [DataPasienController::class, 'selesaiPasien'])->name('selesai.pasien');
 Route::post('/reset-antrian', [DataPasienController::class, 'resetAntrian'])->name('reset.antrian');
-Route::post('/update-data', [DataPasienController::class, 'updateData']); // Menambahkan route untuk update data melalui AJAX
